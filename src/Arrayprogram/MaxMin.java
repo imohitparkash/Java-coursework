10edd9d3aa5f76c20a95b84c1b91f00fdf343050
Mohit Parkash <imohitparkash1@gmail.com>
2026-07-08 11:33:04 +0530

    practicing array

--- src/Arrayprogram/MaxMin.java
+++ src/Arrayprogram/MaxMin.java
@@ -4,12 +4,19 @@ public class MaxMin {
     public static void main(String[] args){
         int arr[]={5,3,9,11,7};
         int max =arr[0],i;
+        int min =arr[0];
         for (i=1;i<arr.length;i++){
-            if (max<arr[i])
-            {
-            max = arr[i];
+            if (max<arr[i]) {
+                max = arr[i];
             }
+            if (min>arr[i]){
+                min =arr[i];
+            }
+
         }
-        System.out.println("maximum: ");
+        System.out.println("maximum: "+ max);
+        System.out.println("minimum: "+ min);
+
+
     }
 }
