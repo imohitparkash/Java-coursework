8422670a245aa6133382e100db7112c4aa5d6d0b
Mohit Parkash <imohitparkash1@gmail.com>
2026-07-08 13:16:32 +0530

    practicing array

---
+++ src/Arrayprogram/unique.java
@@ -0,0 +1,24 @@
+package Arrayprogram;
+
+public class unique
+{
+    public static void main(String[] args) {
+        int arr[]={4,5,2,4,4,4,2,6,4,2,7,4};
+        int i,j;
+        for (i=0;i< arr.length;i++)
+        {
+            int c=0;
+            for (j=0;j< arr.length;j++)
+            {
+                if (arr[i]== arr[j])
+                {
+                    c++;
+                }
+            }
+            if (c==1)
+            {
+                System.out.println(arr[i]+" : " +c);
+            }
+        }
+    }
+}
