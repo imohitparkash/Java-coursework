2a826d8417eb86461dda8f1cff7ff9ae65f869da
Mohit Parkash <imohitparkash1@gmail.com>
2026-07-10 20:55:01 +0530

    practicing

---
+++ src/IfStatement.java
@@ -0,0 +1,19 @@
+public class IfStatement {
+    public static void main(String[] args) {
+        int age=20;
+        int income=200000;
+        int creditScore=815;
+        if (age>18){
+            if (income>150000){
+                if(creditScore>750){
+                    System.out.println("you are eligible for loan");
+                }
+            }
+            else {
+                System.out.println("you are not eligible");
+            }
+        }
+
+
+    }
+}
