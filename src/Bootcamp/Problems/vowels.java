cdb0c316c810128123a0b4a824e5d4cec960cf1e
Mohit Parkash <imohitparkash1@gmail.com>
2026-07-15 23:30:44 +0530

    Just started operators

---
+++ src/Bootcamp/Problems/vowels.java
@@ -0,0 +1,15 @@
+package Bootcamp.Problems;
+
+import java.util.Scanner;
+
+public class vowels {
+    public static void main(String[] args) {
+       String a="";
+       String b=" ";
+        System.out.println(a);
+        System.out.println(a.isBlank());
+        System.out.println(a.isEmpty());
+        System.out.println(b.isEmpty());
+        System.out.println(b.isBlank());
+    }
+}
---
+++ src/GFG/Basics/BitwiseOperator.java
@@ -0,0 +1,15 @@
+package GFG.Basics;
+
+public class BitwiseOperator {
+    public static void main(String[] args) {
+
+         int x=3;
+         int y=6;
+//         And operator
+         System.out.println(x&y);
+         //OR operator
+        System.out.println(x|y);
+        //XOR operator
+        System.out.println(x^y);
+    }
+}
