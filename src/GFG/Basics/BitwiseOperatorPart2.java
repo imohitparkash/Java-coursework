8c4be74b23bc25dff2303797452bc301b08623ca
Mohit Parkash <imohitparkash1@gmail.com>
2026-07-16 23:27:35 +0530

    Just started operators

---
+++ src/GFG/Basics/BitwiseOperatorPart2.java
@@ -0,0 +1,22 @@
+package GFG.Basics;
+
+public class BitwiseOperatorPart2 {
+    public static void main(String[] args) {
+        //not operator
+        int x=5;
+        //System.out.println(~x);
+        //left shift
+        int y=3;
+        //System.out.println(y<<1);
+        //System.out.println(x<<2);
+        //System.out.println(3<<2);
+        //right shift operator
+        int k=33;
+        System.out.println(k>>1);
+        //unsigned right shift
+        int r=-2;
+        System.out.println(r>>>1);
+
+    }
+
+}
