377656431aca6df58f21bc1ab2ecba237fde08ad
Mohit Parkash <imohitparkash1@gmail.com>
2026-07-17 23:49:26 +0530

    Started flow controls...

---
+++ src/GFG/Basics/IfElseStatements.java
@@ -0,0 +1,24 @@
+package GFG.Basics;
+
+import java.util.Scanner;
+
+public class IfElseStatements {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n=sc.nextInt();
+        if (n%2==0){
+            System.out.println("even");
+        }
+        else {
+            System.out.println("odd");
+        }
+    }
+
+}
+
+
+
+
+
+
