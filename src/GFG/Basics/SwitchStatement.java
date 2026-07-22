23fccc9851ce60759a7bc3a1ae03ccaae8cf5695
Mohit Parkash <imohitparkash1@gmail.com>
2026-07-22 23:38:16 +0530

    Learning new everyday...

--- src/GFG/Basics/SwitchStatement.java
+++ src/GFG/Basics/SwitchStatement.java
@@ -8,9 +8,22 @@ public class SwitchStatement {
         int y=0;
         System.out.println("Enter a move: ");
         Scanner sc=new Scanner(System.in);
-        String move=sc.next();
+        Character move=sc.next().charAt(0);
         switch (move){
-
+            case 'L':
+                x--;
+                break;
+            case 'R':
+                x++;
+                break;
+            case 'U':
+                   y++;
+                   break;
+            case 'D':
+                y--;
+                break;
+            default:
+                System.out.println("invalid");
         }
     }
 }
