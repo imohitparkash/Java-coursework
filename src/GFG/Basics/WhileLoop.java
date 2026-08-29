052346005d2ea4cb5412e72bb534d5928c2670a3
imohitparkash <imohitparkash1@gmail.com>
2026-08-29 22:34:45 +0530

    Add while loop

---
+++ src/GFG/Basics/WhileLoop.java
@@ -0,0 +1,11 @@
+package GFG.Basics;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i=0;
+        while(i<10){
+            System.out.println("HEY");
+            i++;
+        }
+    }
+}
