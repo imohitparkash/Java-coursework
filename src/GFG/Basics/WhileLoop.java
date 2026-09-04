e553274574229bb0bf2783a69c564ab2a7435d08
imohitparkash <imohitparkash1@gmail.com>
2026-09-04 22:07:49 +0530

    Add While Loop

--- src/GFG/Basics/WhileLoop.java
+++ src/GFG/Basics/WhileLoop.java
@@ -4,7 +4,7 @@ public class WhileLoop {
     public static void main(String[] args) {
         int i=0;
         while(i<10){
-            System.out.println("HEY");
+            System.out.println(i);
             i++;
         }
     }
