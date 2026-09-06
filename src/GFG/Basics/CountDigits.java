02dfda95ab710df17abda29533d701f27b0c4e1a
imohitparkash <imohitparkash1@gmail.com>
2026-09-06 15:59:45 +0530

    Add Count digits

--- src/GFG/Basics/CountDigits.java
+++ src/GFG/Basics/CountDigits.java
@@ -1,7 +1,18 @@
 package GFG.Basics;
 
+import java.util.Scanner;
+
 public class CountDigits {
     public static void main(String[] args) {
 
+        int ans,n;
+        Scanner sc=new Scanner(System.in);
+        n= sc.nextInt();
+        ans=0;
+        while (n>0){
+            n=n/10;
+            ans++;
+        }
+        System.out.println(ans);
     }
 }
